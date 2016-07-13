ec766859f3530c363d067e50d4a700ffbfc63409
Homa Wong <homawong@gmail.com>
2016-07-12 23:58:18 -0700

    Complete base, only, skip, serial

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -17,3 +17,32 @@ rtest('relative path', 'case-1', (t, path) => {
   t.plan(1);
   t.true(existsSync(filePath), 'should find somefile.txt');
 });
+
+test('case-1', t => {
+  t.pass('work without title.');
+});
+
+// `test.only` test needs to be commented out so other tests can run. :)
+// test.only('only test', 'case-1', t => {
+//   t.pass('only works');
+// });
+
+// test.only('case-1', t => {
+//   t.pass('only without title works.');
+// });
+
+test.skip('skip test', 'case-1', t => {
+  t.pass('skip test works.');
+});
+
+test.skip('case-1', t => {
+  t.pass('skip without title works.');
+});
+
+test.serial('serial', 'case-1', t => {
+  t.pass('serial works.');
+});
+
+test.serial('case-1', t => {
+  t.pass('serial without title works.');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,42 @@
 import { join } from 'path';
-import test, {ContextualTestContext, ContextualTest} from 'ava';
+import test, { ContextualTestContext, ContextualTest, Observable } from 'ava';
 
 export interface FixtureTest {
-  (name: string, fixtureName: string, cb: (t: ContextualTestContext, path: string) => any): ContextualTestContext;
-  only(name: string, fixtureName: string, cb: (t: ContextualTestContext, path: string) => any): ContextualTestContext;
-  skip(name: string, fixtureName: string, cb: (t: ContextualTestContext, path: string) => any): ContextualTestContext;
+  (name: string, fixtureName: string, run: (t: ContextualTestContext, path: string) => Promise<void> | Iterator<any> | Observable | void): void;
+  (fixtureName: string, run: (t: ContextualTestContext, path: string) => Promise<void> | Iterator<any> | Observable | void): void;
+
+  only(name: string, fixtureName: string, run: (t: ContextualTestContext, path: string) => any): void;
+  only(fixtureName: string, run: (t: ContextualTestContext, path: string) => any): void;
+
+  skip(name: string, fixtureName: string, run: (t: ContextualTestContext, path: string) => any): void;
+  skip(fixtureName: string, run: (t: ContextualTestContext, path: string) => any): void;
+
+  serial(name: string, fixtureName: string, run: (t: ContextualTestContext, path: string) => void): void;
+  serial(fixtureName: string, run: (t: ContextualTestContext, path: string) => void): void;
 }
 
 export default function fixture(path: string): FixtureTest {
-  function curry(testfn: (name: string, cb: ContextualTest) => any): any {
+  function curry(testfn: (name: string, run: ContextualTest) => any): any {
     return (
-      title: string,
+      name: string,
       fixtureName: string,
-      cb: (t: ContextualTestContext, path: string) => any
+      run: (t: ContextualTestContext, path: string) => any
     ) => {
+      if (!run) {
+        // name is optional
+        run = fixtureName as any;
+        fixtureName = name;
+      }
+
       const fixturePath = join(path, fixtureName);
-      return testfn(`${title} (fixture: ${fixtureName})`, t => {
-        return cb(t, fixturePath);
+      return testfn(`${name ? name + ' ' : ''}(fixture: ${fixtureName})`, t => {
+        return run(t, fixturePath);
       });
     };
   }
   let result: any = curry(test);
   result.only = curry(test.only);
   result.skip = curry(test.skip);
+  result.serial = curry(test.serial);
   return result as FixtureTest;
 }
