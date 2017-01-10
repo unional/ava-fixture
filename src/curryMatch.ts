c41b7c6a0ab5f43805994ac7d5bc6ed513c845f9
Homa Wong <homawong@gmail.com>
2017-01-10 14:15:43 -0800

    Add init baseline TODO

--- src/curryMatch.ts
+++ src/curryMatch.ts
@@ -1,7 +1,7 @@
 import { getLogger } from 'aurelia-logging'
 import { ContextualTestContext } from 'ava'
 import { compare } from 'dir-compare'
-import { readFileSync } from 'fs'
+import { readFileSync, existsSync } from 'fs'
 import { join, relative } from 'path'
 
 const logger = getLogger('fixture')
@@ -11,6 +11,10 @@ export function curryMatch(baselinePath: string, resultPath: string, t: Contextu
   // Use this to do relative() with filename get good relative path.
   const cwd = process.cwd()
   return function match() {
+    if (!existsSync(baselinePath)) {
+      // TODO
+      // Copy from result folder to baseline folder to start as a baseline
+    }
     return compare(baselinePath, resultPath)
       .then((res) => {
         t.is(res.differences, 0, 'Some files / folders mismatch')
